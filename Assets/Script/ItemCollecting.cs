6d2862765d093b3cfece9b7a61dce677feaa3b73
Jinsab <kimjinou116@naver.com>
2019-08-21 07:41:02 +0900

    [Create] Cherry Collecting
    Script
     [+] ItemCollecting
     [+] ScoreManager[Incomplete]
    
    Animation
     [+] Cherry_Idle

---
+++ Assets/Script/ItemCollecting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollecting : MonoBehaviour
+{
+    bool collecting = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collecting)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collecting = true;
+    }
+}
