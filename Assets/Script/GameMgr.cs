bbc9e619a2e94de76f42123bdbbaa99204b89702
Jinsab <kimjinou116@naver.com>
2019-09-09 09:02:09 +0900

    [Update] GameMgr
    PlayerPrefs System [+]

---
+++ Assets/Script/GameMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMgr : MonoBehaviour
+{
+    public InputField inputScore;
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("Score", int.Parse(inputScore.text));
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("Score"))
+        {
+            inputScore.text = PlayerPrefs.GetInt("Score").ToString();
+        }
+    }
+}
